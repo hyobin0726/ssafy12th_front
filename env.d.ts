68074c15155db5a8cf1d24d6f392ce4c6b4fa708
hyobin <hyobin0726@naver.com>
2024-11-07 17:52:21 +0900

    feat: 리뷰 작성 s3 추가

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,18 @@
 /// <reference types="vite/client" />
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
-  const component: DefineComponent<unknown, unknown, any>
+  const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+interface ImportMetaEnv {
+  readonly VITE_AWS_S3_REGION: string
+  readonly VITE_AWS_S3_BUCKET_NAME: string
+  readonly VITE_AWS_S3_ACCESS_KEY: string
+  readonly VITE_AWS_S3_SECRET_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
--- src/components/Review/ReviewWrite.vue
+++ src/components/Review/ReviewWrite.vue
@@ -4,7 +4,7 @@
       <div class="flex justify-between items-center mb-4">
         <Back @click="closeModal" class="w-6 h-6" />
         <span class="text-xl">새 리뷰 작성</span>
-        <button class="text-xl">다음</button>
+        <button class="text-xl" @click="submitImage">다음</button>
       </div>
 
       <div class="flex justify-center items-center h-screen">
@@ -15,22 +15,31 @@
         >
           사진 추가
         </label>
-        <!-- 이미지 미리보기 (전체 영역을 채움) -->
-        <div v-if="previews.length > 0" class="relative w-full h-full overflow-hidden rounded-md">
-          <img
-            v-for="(preview, index) in previews"
-            :key="index"
-            :src="preview"
-            alt="미리보기 이미지"
-            class="absolute inset-0 w-full h-full object-cover"
-          />
-          <label
-            for="inputFile"
-            class="absolute bottom-2 right-2 font-semibold p-4 rounded-2xl bg-[#A8B087] text-white"
-          >
-            사진 추가
-          </label>
-        </div>
+        <Swiper
+          v-if="previews.length > 0"
+          class="relative w-full h-full overflow-hidden rounded-md"
+          :slidesPerView="1"
+          :loop="true"
+          :navigation="previews.length > 1 ? navigationOptions : false"
+        >
+          <SwiperSlide v-for="(preview, index) in previews" :key="index">
+            <img :src="preview" alt="미리보기 이미지" class="absolute inset-0 w-full h-full object-cover" />
+            <button
+              class="absolute top-1 right-1 bg-red-500 text-white w-7 h-7 rounded-full flex justify-center items-center"
+              @click="removeImage(index)"
+            >
+              x
+            </button>
+            <label
+              for="inputFile"
+              class="absolute bottom-2 right-2 font-semibold p-4 rounded-2xl bg-[#A8B087] text-white"
+            >
+              사진 추가
+            </label>
+          </SwiperSlide>
+          <div v-if="previews.length > 1" class="swiper-button-next text-[#DAB692]"></div>
+          <div v-if="previews.length > 1" class="swiper-button-prev text-[#DAB692]"></div>
+        </Swiper>
       </div>
 
       <input
@@ -43,16 +52,25 @@
         accept="image/*"
       />
     </div>
+    <ReviewWriteContent :isOpen="isNextModalOpen" :imageUrls="uploadedUrls" @close="isNextModalOpen = false" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import Back from '@/assets/Nav/Back.svg'
-
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import ReveiewWriteContent from './ReveiewWriteContent.vue'
 export default defineComponent({
+  name: 'ImageUploader',
   components: {
     Back,
+    Swiper,
+    SwiperSlide,
+    ReveiewWriteContent,
   },
   props: {
     isVisible: {
@@ -60,37 +78,89 @@ export default defineComponent({
       required: true,
     },
   },
-  emits: ['close'],
-  methods: {
-    closeModal() {
-      this.$emit('close')
-    },
-  },
-  setup() {
+  emits: ['close', 'submitImages'],
+
+  setup(props, { emit }) {
+    const isNextModalOpen = ref(false)
+    const navigationOptions: any = { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }
     const fileInput = ref<HTMLInputElement | null>(null)
     const previews = ref<string[]>([])
+    const uploadedUrls = ref<string[]>([])
+    // S3 클라이언트 설정
+    const s3Client = new S3Client({
+      region: import.meta.env.VITE_AWS_S3_REGION,
+      credentials: {
+        accessKeyId: import.meta.env.VITE_AWS_S3_ACCESS_KEY,
+        secretAccessKey: import.meta.env.VITE_AWS_S3_SECRET_KEY,
+      },
+    })
+    // 파일을 S3에 업로드하는 함수
+    const uploadImageToS3 = async (file: File): Promise<string | null> => {
+      const fileName = `image_${Date.now()}_${file.name}`
+      const command = new PutObjectCommand({
+        Bucket: import.meta.env.VITE_AWS_S3_BUCKET_NAME,
+        Key: fileName,
+        Body: file,
+        ContentType: file.type,
+      })
 
-    // 파일 업로드 핸들러
-    const handleFileUpload = (event: Event) => {
+      try {
+        // console.log('업로드 시작:', command)
+        const response = await s3Client.send(command)
+        // console.log('업로드 성공:', response)
+        return `https://${import.meta.env.VITE_AWS_S3_BUCKET_NAME}.s3.${import.meta.env.VITE_AWS_S3_REGION}.amazonaws.com/${fileName}`
+      } catch (error) {
+        console.error('S3 업로드 오류:', error)
+        return null
+      }
+    }
+    // 파일 업로드 및 미리보기 처리 함수
+    const handleFileUpload = async (event: Event) => {
       const target = event.target as HTMLInputElement
       const files = target.files
       if (files) {
-        Array.from(files).forEach((file) => {
-          const reader = new FileReader()
-          reader.onload = (e) => {
-            if (e.target && e.target.result) {
-              previews.value.push(e.target.result as string) // 미리보기 배열에 이미지 추가
+        const fileArray = Array.from(files)
+        for (const file of fileArray) {
+          const s3Url = await uploadImageToS3(file)
+          if (s3Url) {
+            uploadedUrls.value.push(s3Url) // S3 URL 저장
+            const reader = new FileReader()
+            reader.onload = (e) => {
+              if (e.target && e.target.result) {
+                previews.value.push(e.target.result as string)
+              }
             }
+            reader.readAsDataURL(file)
           }
-          reader.readAsDataURL(file) // 이미지 파일을 Data URL로 변환
-        })
+        }
       }
+    }
+
+    const removeImage = (index: number) => {
+      previews.value.splice(index, 1)
+      uploadedUrls.value.splice(index, 1)
+    }
+    const submitImage = () => {
+      emit('submitImages', uploadedUrls.value)
+      isNextModalOpen.value = true
+      console.log('이미지 업로드 완료:', uploadedUrls.value)
+    }
+    const closeModal = () => {
+      emit('close')
+      previews.value = []
+      uploadedUrls.value = []
     }
 
     return {
       fileInput,
+      closeModal,
       handleFileUpload,
       previews,
+      navigationOptions,
+      removeImage,
+      submitImage,
+      isNextModalOpen,
+      uploadedUrls,
     }
   },
 })
