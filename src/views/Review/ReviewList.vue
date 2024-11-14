4c1c400b875baa416c8b691fd8f80bc3ac2e1b79
hyobin0726 <hyobin0726@naver.com>
2024-11-15 05:54:03 +0900

    fix: 리뷰리스트 디자인 수정

--- src/views/Review/ReviewList.vue
+++ src/views/Review/ReviewList.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="flex h-screen">
     <Nav class="w-1/4 h-screen" />
-    <div class="w-3/4 overflow-y-scroll">
-      <template v-if="reviews.length > 0">
-        <ReviewItem v-for="review in reviews" :key="review.reviewId" :review="review" />
-      </template>
-      <p v-else class="text-center text-gray-500 flex justify-center items-center h-screen">작성된 리뷰가 없습니다.</p>
+    <div class="w-3/4 overflow-y-scroll p-14">
+      <div class="space-y-5">
+        <template v-if="reviews.length > 0">
+          <ReviewItem v-for="review in reviews" :key="review.reviewId" :review="review" />
+        </template>
+        <p v-else class="text-center text-gray-500 flex justify-center items-center h-screen">
+          작성된 리뷰가 없습니다.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +29,6 @@ export default defineComponent({
       try {
         const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/v1/reviews/list`)
         reviews.value = response.data
-        // console.log('리뷰 데이터를 가져왔습니다:', reviews.value)
       } catch (error) {
         console.error('리뷰 데이터를 가져오는데 실패했습니다:', error)
       }
